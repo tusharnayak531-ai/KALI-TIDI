9b7feff404f62730aad14743f2983048a64e4261
tusharnayak531-ai <tusharnayak531@gmail.com>
2026-09-18 13:36:39 +0530

    Add V8 site support file

---
+++ v8site/js/config.js
@@ -0,0 +1,7 @@
+window.NS_CONFIG = {
+  company: 'N&S DEVELOPERS',
+  city: 'Rajkot, Gujarat',
+  email: 'nsdeveloper.in@gmail.com',
+  formEndpoint: '',
+  instagram: 'https://instagram.com/nsdevelopers.in'
+};
